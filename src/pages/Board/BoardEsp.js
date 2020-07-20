875b0293a270be3125b7709054f42f2c3af8075c
Barry Rollan <barryrollan@macbook-pro.lan>
2020-07-20 18:42:51 -0400

    correcting errors for deploy on netlify

--- src/pages/Board/BoardEsp.js
+++ src/pages/Board/BoardEsp.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import NavBar from '../../components/NavBarEsp';
+import NavBar from '../../components/NavBar';
 import { Link } from 'react-router-dom';
 import Footer from '../../components/Footer';
 import bios from '../../data/biosEsp';
