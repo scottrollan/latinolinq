fe27c983f4cb37afdd05bab3a01c68adb4205426
Scott Rollan <b.scott.rollan@gmail.com>
2022-03-14 20:12:55 -0400

    added two more job postings

---
+++ src/data/jobs.js
@@ -0,0 +1,419 @@
+import React from 'react';
+import styles from '../pages/Join/Join.module.scss';
+
+const jobPosts = [
+  {
+    position: 'Board Member',
+    details: (
+      <div>
+        <div className={styles.twoCol}>
+          <div style={{ flex: 1, marginRight: '3rem' }}>
+            <div className={styles.subHeader}>EXPECTATIONS OF THE BOARD</div>
+            <p>
+              Latino LinQ mission: increasing equity and promoting wellbeing for
+              Latinx LGBTQ+ communities in the South.
+            </p>
+            <p>
+              As the highest leadership body of the organization and to satisfy
+              its fiduciary duties, the board is responsible for:
+            </p>
+            <ul>
+              <li>determining the mission and purposes of the organization</li>
+              <li>strategic and organizational planning</li>
+              <li>
+                ensuring strong fiduciary oversight and financial management
+              </li>
+              <li>fundraising and resource development</li>
+              <li>
+                approving and monitoring Latino LinQ’s programs and services
+              </li>
+              <li>enhancing Latino LinQ’s public image</li>
+              <li>
+                assessing its own performance as the governing body of Latino
+                LinQ
+              </li>
+            </ul>
+          </div>
+          <div style={{ flex: 1 }}>
+            <div className={styles.subHeader}>
+              EXPECTATIONS OF INDIVIDUAL BOARD MEMBERS
+            </div>
+            <p>Each individual board member is expected to:</p>
+            <ul>
+              <li>
+                know the organization’s mission, policies, programs, and needs
+              </li>
+              <li>
+                serve as active advocates and ambassadors for the organization
+                and fully engage in identifying and securing the financial
+                resources and partnerships necessary for Latino LlinQ to advance
+                its mission
+              </li>
+              <li>
+                leverage connections, networks, and resources to develop
+                collective action to fully achieve Latino LinQ’s mission
+              </li>
+              <li>give a meaningful personal financial donation</li>
+              <li>
+                help identify personal connections that can benefit the
+                organization’s fundraising and reputational standing, and can
+                influence public policy
+              </li>
+              <li>
+                prepare for, attend, and conscientiously participate in board
+                meetings
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <div className={styles.subHeader}>
+            BOARD MEMBERS ARE ALSO EXPECTED TO
+          </div>
+          <ul>
+            <li>
+              follow the organization’s bylaws, policies, and board resolutions
+            </li>
+            <li>
+              sign an annual conflict-of-interest disclosure and update it
+              during the year if necessary, as well as disclose potential
+              conflicts before meetings and actual conflicts during meetings
+            </li>
+            <li>maintain confidentiality about all internal matters</li>
+          </ul>
+        </div>
+      </div>
+    ),
+  },
+  {
+    position: 'Summit Manager',
+    details: (
+      <div>
+        <div>
+          <span style={{ textDecoration: 'underline' }}>Posting date:</span>
+          &nbsp;March 14, 2022
+        </div>
+        <div>
+          <span style={{ textDecoration: 'underline' }}>
+            Anticipated start date:
+          </span>
+          &nbsp;ASAP
+        </div>
+        <div>
+          <span style={{ textDecoration: 'underline' }}>Pay rate:</span>
+          &nbsp;$30/hour
+        </div>
+        <div style={{ fontStyle: 'italic' }}>
+          This is a part-time, temporary position with a flexible work schedule.
+          Summit Manager will dedicate between 8-10 hours per week to this
+          project. We expect deliverables to be completed by the end of December
+          2022.
+        </div>
+        <div>
+          <div className={styles.subHeader}>About Latino LinQ</div>
+          <div>
+            Latino LinQ is a nonprofit organization in Georgia advocating for a
+            world in which Latinx LGTBQ+ individuals and their families can live
+            authentically, with dignity and respect. Our mission is to increase
+            equity and promote wellbeing for Latinx LGBTQ+ communities
+            throughout the South.
+          </div>
+        </div>
+        <div>
+          <div className={styles.subHeader}>About the position</div>
+          <div>
+            We are seeking a Summit Manager for a grant-funded position who is
+            detail-oriented, self-driven, and holds a passion for racial
+            justice, HIV prevention, mental and sexual health. This individual
+            will manage the third iteration of Latino LinQ’s LGBTQ Summit. As a
+            volunteer-led and operated organization, once a year Latino LinQ
+            brings together Georgia's Latinx LGBTQ community for a one day
+            Summit with workshops and speakers on topics impacting our
+            wellbeing. This Summit is the only Latinx LGBTQ-led educational
+            event in Georgia. This year’s Summit will be centered around
+            buildingracial and gender justice and will rely heavily on building
+            partnerships with Black and Transgender-led organizations in the
+            metro-Atlanta region. Alongside the Latino LinQ board, the Summit
+            Manager will plan a convening to build bridges between LGBTQ-Latinx
+            and LGBTQ-Black communities and that strengthen relationships
+            between LGB and Transgender folks. The Latino LinQ board believes
+            that in order to address key issues facing both Black and Latinx
+            LGBTQ individuals (ie. HIV, over-policing, racism, transphobia,
+            inadequate access to vital resources), reconciling our differences
+            and fortifying unity is necessary.
+          </div>
+        </div>
+        <div>
+          <div className={styles.subHeader}>Duties and Responsibilities</div>
+          <div>
+            <ul>
+              <li>
+                Recruit and coordinate a racial justice and a gender justice
+                planning group for the Summit.
+              </li>
+              <li>
+                Alongside the planning group, coordinate speakers and develop
+                agenda.
+              </li>
+              <li>
+                Manage promotion, registration, and general correspondence with
+                potential attendees.
+              </li>
+              <li>Solicit sponsorships and donations.</li>
+              <li>
+                Manage logistics for the Summit, including but not limited to:
+                managing the project budget, advertising, confirming speakers,
+                securing meeting space, and coordinating technology,
+                interpretation, food, and lodging.
+              </li>
+              <li>
+                Document the planning process alongside the Latino LinQ board.
+              </li>
+              <li>
+                Incorporate evaluations into the Summit and host a debrief with
+                the Latino LinQ board and community stakeholders.
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <div className={styles.subHeader}>Knowledge, Skills, Abilities</div>
+          <div>
+            <ul>
+              <li>
+                Strong knowledge of event planning and project management in
+                social justice settings.
+              </li>
+              <li>
+                Experience with grassroots fundraising and/or corporate giving.
+              </li>
+              <li>Ability to work with diverse groups.</li>
+              <li>
+                Strong interpersonal skills and commitment to cultural
+                responsiveness.
+              </li>
+              <li>
+                Knowledge of Latinx-LGBTQ issues and issues related to racial
+                and gender justice.
+              </li>
+              <li>
+                Detail-oriented with excellent organizing and communications
+                skills.
+              </li>
+              <li>
+                Strong writing and computer skills (Word, Excel, Powerpoint,
+                etc.)
+              </li>
+              <li>Some flexibility with work hours.</li>
+              <li>Ability to self-manage and report to remote supervisors.</li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <div className={styles.subHeader}> Preferred Qualifications</div>
+          <ul>
+            <li>Spanish and English fluency preferred.</li>
+            <li>
+              Strong connections to the LGBTQ+, Latinx, Black and Transgender
+              communities.
+            </li>
+            <li>
+              Minimum of three years of event planning or project management
+              experience in a social justice setting.
+            </li>
+          </ul>
+        </div>
+        <div>
+          <div className={styles.subHeader}>How to apply</div>
+          <p>
+            Latino LinQ is an Equal Opportunity Employer and does not
+            discriminate against any employee or applicant for employment
+            because of race, color, sex, age, national origin, religion, sexual
+            orientation, gender identity, status as a veteran, and basis of
+            disability or any other federal, state, or local protected class.
+            People with strong connections to the Latino, LGBTQ community, and
+            people living with HIV are strongly encouraged to apply.
+          </p>
+          <p>
+            Please send a cover letter, resume, and two references to{' '}
+            <span style={{ fontWeight: 'bold' }}>latinolinq@gmail.com</span>{' '}
+            with “HIV Services Coordinator” in the subject line.{' '}
+            <span style={{ fontWeight: 'bold' }}>
+              This position is open until filled.
+            </span>
+          </p>
+        </div>
+      </div>
+    ),
+  },
+  {
+    position: 'Job Post: HIV Services Coordinator',
+    details: (
+      <div>
+        <div>
+          <span style={{ textDecoration: 'underline' }}>Posting date:</span>
+          &nbsp;January 28, 2022
+        </div>
+        <div>
+          <span style={{ textDecoration: 'underline' }}>
+            Anticipated start date:
+          </span>
+          &nbsp;ASAP
+        </div>
+        <div>
+          <span style={{ textDecoration: 'underline' }}>Pay rate:</span>
+          &nbsp;$25/hour
+        </div>
+        <div style={{ fontStyle: 'italic' }}>
+          This is a part-time, temporary position with a flexible work schedule
+          between 8-10 hours per week. We expect contract deliverables to be
+          completed by the end of December 2022.
+        </div>
+        <div>
+          <div className={styles.subHeader}>About Latino LinQ</div>
+          <div>
+            Latino LinQ is a nonprofit organization in Georgia advocating for a
+            world in which Latinx LGTBQ+ individuals and their families can live
+            authentically, with dignity and respect. Our mission is to increase
+            equity and promote wellbeing for Latinx LGBTQ+ communities
+            throughout the South.
+          </div>
+        </div>
+        <div>
+          <div className={styles.subHeader}>About the position</div>
+          <div>
+            We are seeking an HIV-Services Coordinator for a grant-funded
+            position who is detail-oriented, self-driven, and holds a passion
+            for HIV prevention and sexual health. This individual will connect
+            Atlanta’s LGBTQ Latino community to HIV-testing, linkage to PrEP or
+            HIV-care services. As a volunteer-led and operated organization,
+            Latino LinQ offers its HIV testing and referral services free to the
+            community in diverse settings across the Metro-Atlanta region. By
+            partnering with Positive Impact Health Centers (PIHC), this position
+            will strengthen Latino LinQ’s culturally responsive HIV testing
+            program, while educating the greater Atlanta Latino community on HIV
+            and sexual health. The ideal candidate must work remotely and
+            in-person, as COVID-19 allows. Latino LinQ will provide access to
+            PPE and rapid COVID-19 testing to staff attending in-person events.
+          </div>
+        </div>
+        <div>
+          <div className={styles.subHeader}>Duties and Responsibilities</div>
+          <div>
+            <ul>
+              <li>
+                Identify community-based sites to provide HIV testing to the
+                Latinx community.
+              </li>
+              <li>
+                Provide confidential, rapid HIV-testing in community-based
+                settings.
+              </li>
+              <li>
+                Connect clients to appropriate agencies for their HIV
+                prevention/care needs.
+              </li>
+              <li>
+                Manage client data with care and create a de-identified database
+                of clients.
+              </li>
+              <li>Assist with reporting data to grant funders.</li>
+              <li>
+                Enhance engagement in care by assisting clients with appointment
+                reminders and general encouragement.
+              </li>
+              <li>
+                Follow-up with clients to overcome barriers to getting and
+                remaining in care.
+              </li>
+              <li>
+                Facilitate linkage to mental health, housing, legal, or other
+                needed support services.
+              </li>
+              <li>Train and oversee HIV-testing volunteers.</li>
+              <li>
+                Amplify Latino LatinQ’s testing services/events on social media.
+              </li>
+              <li>
+                Provide information at community health fairs, festivals, or
+                other community events.
+              </li>
+              <li>Other duties as assigned.</li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <div className={styles.subHeader}>Knowledge, Skills, Abilities</div>
+          <div>
+            <ul>
+              <li>Knowledge of HIV transmission, prevention, and care.</li>
+              <li>
+                A general understanding of medication adherence and barriers to
+                adherence.
+              </li>
+              <li>
+                Knowledge of sexually transmitted infections (STIs), their
+                connection to HIV, their impact on the health of PLHIV.
+              </li>
+              <li>Ability to work with diverse groups.</li>
+              <li>
+                Strong interpersonal skills and commitment to cultural
+                responsiveness.
+              </li>
+              <li>
+                Knowledge of Latinx-LGBTQ issues and issues related to social
+                justice.
+              </li>
+              <li>
+                Detail-oriented with excellent organizing and communications
+                skills.
+              </li>
+              <li>
+                Strong writing and computer skills (Word, Excel, Powerpoint,
+                etc.)
+              </li>
+              <li>Flexibility regarding work hours.</li>
+              <li>Ability to self-manage and report to remote supervisors.</li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <div className={styles.subHeader}> Preferred Qualifications</div>
+          <ul>
+            <li>Spanish and English fluency</li>
+            <li>Strong connections to the LGBTQ+, Latinx Community</li>
+            <li>Life experience with HIV/AIDS</li>
+            <li>
+              Two years of experience as an HIV-testing Counselor or HIV-Case
+              Manager OR two years of experience serving Latinx people in a
+              direct service agency or other health setting
+            </li>
+          </ul>
+        </div>
+        <div>
+          <div className={styles.subHeader}>How to apply</div>
+          <p>
+            Latino LinQ is an Equal Opportunity Employer and does not
+            discriminate against any employee or applicant for employment
+            because of race, color, sex, age, national origin, religion, sexual
+            orientation, gender identity, status as a veteran, and basis of
+            disability or any other federal, state, or local protected class.
+            People with strong connections to the Latino, LGBTQ community, and
+            people living with HIV are strongly encouraged to apply.
+          </p>
+          <p>
+            Please send a cover letter, resume, and two references to{' '}
+            <span style={{ fontWeight: 'bold' }}>latinolinq@gmail.com</span>{' '}
+            with “HIV Services Coordinator” in the subject line.{' '}
+            <span style={{ fontWeight: 'bold' }}>
+              This position is open until filled and with the application
+              deadline of Feb 16th.
+            </span>
+          </p>
+        </div>
+      </div>
+    ),
+  },
+];
+
+export default jobPosts;
